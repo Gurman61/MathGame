c52dccc3c9fb05a355d37d5c2deab4b430faf693
Gurman61 <78517005+Gurman61@users.noreply.github.com>
2021-07-21 20:47:08 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -12,23 +12,15 @@ document.getElementById("player2_answer").innerHTML="Answer turn "+player2_name;
 
 function send()
 {
-    get_word=document.getElementById("word").value;
-    word=get_word.toLowerCase();
-    console.log("word"+word);
+   number1=document.getElementById("word1").value;
+   number2=document.getElementById("word2").value;
+   actual_answer=parseInt(number1)*parseInt(number2);
+   question_number="<h4>"+number1+"X"+number2+"</h4>";
+   input_box="<br> Answer: <input type='text' id='input_check_box'>";
+   check_button="<br><br><button onclick='check()' class='btn-info'>Check</button>";
+   row=question_number+input_box+check_button;
 
-    charAt1=word.charAt(1);
-    console.log(charAt1);
-    
-    length_divide_2=Math.floor(word.length/2);
-    charAt2=word.charAt(length_divide_2);
-    console.log(charAt2);
-
-    length_minus_1=word.length-1;
-    charAt3=word.charAt(length_minus_1);
-    console.log(charAt3);
-
-    remove_charAt1=word.replace(charAt1,"_");
-    remove_charAt2=remove_charAt1.replace(charAt2,"_");
-    remove_charAt3=remove_charAt2.replace(charAt3,"_");
-    console.log(remove_charAt3);
+   document.getElementById("output").innerHTML=row;
+   document.getElementById("number1").value="";
+   document.getElementById("number2").value="";
 }
